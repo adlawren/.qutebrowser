a759082c64af9fcf4d4be4f2f2cdae080847d73d
A.L <adlawren010@gmail.com>
2018-09-02 16:38:20 -0600

    Change keybindings

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
-colemak_remappings = {'s': 'r', 'd': 's', 'f': 't', 'g': 'd', 'e': 'f', 'r': 'p', 't': 'g', 'n': 'k', 'j': 'n', 'k': 'e', 'l': 'i', ';': 'o', 'y': 'j', 'u': 'l', 'i': 'u', 'o': 'y', 'p': ';', 'S': 'R', 'D': 'S', 'F': 'T', 'G': 'D', 'E': 'F', 'R': 'P', 'T': 'G', 'N': 'K', 'J': 'N', 'K': 'E', 'L': 'I', ':': 'O', 'Y': 'J', 'U': 'L', 'I': 'U', 'O': 'Y', 'P': ':'}
+def remap_in_all_modes(remappings):
+    for mode in c.bindings.default:
+        new_dict = {}
+        for key in c.bindings.default[mode]:
+            if key[0] == '<':
+                # ignore meta-characters
+                new_dict[key] = c.bindings.default[mode][key]
+            else:
+                new_key = ""
+                for key_character in key:
+                    new_key_character = key_character
+                    if key_character in remappings:
+                        new_key_character = remappings[key_character]
+                    new_key += new_key_character
+                new_dict[new_key] = c.bindings.default[mode][key]
+        c.bindings.default[mode] = new_dict
 
-for mode in c.bindings.default:
-    new_dict = {}
-    for key in c.bindings.default[mode]:
-        if key[0] == '<':
-            # ignore meta-characters
-            new_dict[key] = c.bindings.default[mode][key]
-        else:
-            new_key = ""
-            for key_character in key:
-                new_key_character = key_character
-                if key_character in colemak_remappings:
-                    new_key_character = colemak_remappings[key_character]
-                new_key += new_key_character
-            new_dict[new_key] = c.bindings.default[mode][key]
-    c.bindings.default[mode] = new_dict
+remappings = {'i': 's', 'h': 'n', 'j': 'e', 'k': 'u', 'l': 'i', 'n': 'l', 's': 'j', 'u': 'h', 'I': 'S', 'H': 'N', 'J': 'E', 'K': 'U', 'L': 'I', 'N': 'L', 'S': 'J', 'U': 'H'}
+
+remap_in_all_modes(remappings)
